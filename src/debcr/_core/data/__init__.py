6349eec3f96983e7a0c46445881c520cce344ace
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-02-26 10:15:11 +0100

    add npz loader; merge code

--- src/debcr/_core/data/__init__.py
+++ src/debcr/_core/data/__init__.py
@@ -1,10 +1,7 @@
-from .loaders import get_loader
+from .loaders import get_loader, get_format
 from .process import patchify, rescale_min_max
-from .utils import get_format
 
 __all__ = [
-    "get_loader",
-    "patchify",
-    "rescale_min_max",
-    "get_format"
+    "get_loader", "get_format",
+    "patchify", "rescale_min_max"
 ]
--- src/debcr/_core/data/loaders.py
+++ src/debcr/_core/data/loaders.py
@@ -14,19 +14,23 @@ def register_loader(*formats):
         return func
     return decorator
 
-# TIFF Loader
 @register_loader("tiff", "tif")
 def load_tiff(file_path: str) -> np.ndarray:
     """Loads a TIFF or TIF image (stack)."""
     img_stack = tiff.imread(file_path)
     return img_stack
 
-# PNG, JPG, JPEG Loader
 @register_loader("png", "jpg", "jpeg")
 def load_png_jpeg(file_path: str) -> np.ndarray:
     """Loads a PNG, JPG, or JPEG image (stack)."""
     img_stack = skio.imread(file_path)
     return img_stack
+
+@register_loader("npz")
+def load_npz(file_path: str):
+    """Loads a multi-array NPZ archive."""
+    marr_data = np.load(file_path)
+    return marr_data
 
 def get_loader(file_format):
     """Returns the correct loader function for the given format."""
@@ -34,3 +38,16 @@ def get_loader(file_format):
     if file_format not in FORMAT_REGISTRY:
         raise ValueError(f"Unsupported format: {file_format}. Available formats: {list(FORMAT_REGISTRY.keys())}")
     return FORMAT_REGISTRY[file_format]
+
+def get_format(file_path: str) -> str:
+    """Returns the file format for the give file path."""
+    _,file_format = os.path.splitext(file_path)
+    return file_format[1:] # truncate preceding dot
+
+'''
+def is_supported_format(file_format):
+    """Returns True if the loader for the given format is implemented."""
+    file_format = file_format.lower()
+    is_supported = file_format in FORMAT_REGISTRY
+    return is_supported
+'''
