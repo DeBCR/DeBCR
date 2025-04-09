9145d5264ebf540033a10b283ea1c16fc1cb9223
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-04-09 13:19:33 +0200

    remove imports

--- src/debcr/__init__.py
+++ src/debcr/__init__.py
@@ -1,14 +1,1 @@
 __version__ = "0.1.0"
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-from . import data
-from . import model
-from . import config
-
-__all__ = [
-    "data",
-    "model",
-    "config"
-]
