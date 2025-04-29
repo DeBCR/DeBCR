99230bb6faf5d82f5247ae53c64ea2e9dd182b29
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-04-29 12:55:45 +0200

    alias core fn import as private

--- src/debcr/config.py
+++ src/debcr/config.py
@@ -1,16 +1,16 @@
-from ._core import config
+from ._core import config as _config
 
 def load(config_path: str = None):
 
-    config_obj = config.Config()
+    config = _config.Config()
     
     if config_path is not None:
-        config_obj = config_obj.from_yaml(config_path)
+        config = config.from_yaml(config_path)
     
-    return config_obj.to_dict()
+    return config.to_dict()
 
 def save(config_dict: dict, config_path: str = 'config.yaml'):
     
-    config_obj = config.Config().from_dict(config_dict)
+    config = _config.Config().from_dict(config_dict)
     
-    return config_obj.to_yaml(config_path)
+    return config.to_yaml(config_path)
