7e65aba2b9740d97efbb64b8aa0a14c7f0c4d009
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-05-14 18:41:49 +0200

    add writers for tif(f),jp(e)g,png

--- src/debcr/_core/data/file_writers.py
+++ src/debcr/_core/data/file_writers.py
@@ -1,7 +1,9 @@
 import os
+import warnings
+
 import numpy as np
-#import tifffile as tiff
-#import skimage.io as skio
+import tifffile as tiff
+import skimage.io as skio
 
 # Registry for supported formats
 FORMAT_REGISTRY = {}
@@ -13,19 +15,30 @@ def register_writer(*formats):
             FORMAT_REGISTRY[fmt.lower()] = func
         return func
     return decorator
-'''
-@register_loader("tiff", "tif")
-def load_tiff(file_path: str) -> np.ndarray:
-    """Loads a TIFF or TIF image (stack)."""
-    img_stack = tiff.imread(file_path)
-    return img_stack
 
-@register_loader("png", "jpg", "jpeg")
-def load_png_jpeg(file_path: str) -> np.ndarray:
-    """Loads a PNG, JPG, or JPEG image (stack)."""
-    img_stack = skio.imread(file_path)
-    return img_stack
-'''
+@register_writer("tiff", "tif")
+def write_tiff(file_path: str, data: np.ndarray):
+    """Writes a TIFF or TIF image (stack)."""
+    tiff.imsave(file_path, data)
+
+@register_writer("png", "jpg", "jpeg")
+def write_png_jpeg(file_path: str, data: np.ndarray):
+    """Writes a PNG, JPG, or JPEG image (stack)."""
+    fmt = get_format(file_path)
+    fmt = fmt.upper()
+    # these formats require uint8 data, so input will be rescaled to [0,255]
+    warnings.warn("The data will be rescaled to [0,255] and casted to 'uint8' due to the file format " + fmt);
+    data_01 = (data - data.min()) / (data.max() - data.min() + 1e-16)
+    data_01 = np.clip(data_01, 0, 1)
+    data_uint = data.astype(np.uint8)
+    
+    # these formats do not natively support multi-arrays, so slices will be saved as individual images
+    warnings.warn("The data slices will be saved as individual images due to the file format " + fmt);
+    file_path_pref,file_format = os.path.splitext(file_path)
+    for i in range(data_uint.shape[0]):
+        slice_file_path = f'{file_path_pref}_{str(i+1).zfill(4)}{file_format}'
+        skio.imsave(slice_file_path, data_uint[i])
+    
 @register_writer("npy")
 def write_npy(file_path: str, data: np.ndarray):
     """Writes a single-array NPY file."""
