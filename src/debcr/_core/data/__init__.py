e1c0f6a51306c1145dc4a5ae8aa98fcaeb6246a6
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-05-14 10:55:13 +0200

    make file writers visible func

--- src/debcr/_core/data/__init__.py
+++ src/debcr/_core/data/__init__.py
@@ -1,9 +1,10 @@
 from .file_loaders import get_loader, get_format
+from .file_writers import get_writer, get_format
 from .process import crop, stitch, normalize
 from .show import show_slices
 
 __all__ = [
-    "get_loader", "get_format",
+    "get_loader", "get_writer", "get_format",
     "crop", "stitch", "normalize",
     "show_slices"    
 ]
