28ca79848be84f0ee992311fdf4322e3ff17b7cf
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2025-05-14 18:42:45 +0200

    add param to transpose subplots positions (i.e. data <-> slices)

--- src/debcr/_core/data/show.py
+++ src/debcr/_core/data/show.py
@@ -1,28 +1,35 @@
 import random
 import matplotlib.pyplot as plt
 
-def show(data: list, slices=[-1], titles=[], figsize=(5,5), cmap='inferno', show_titles=True, show_ids=True):
+def show(data: list, slices=[-1], titles=[], figsize=(5,5), cmap='inferno', show_titles=True, show_ids=True, transpose=False):
 
-    ns = len(slices)
-    for idx in range(ns):
-        slices[idx] = slices[idx] if slices[idx] != -1 else random.randint(0, data[0].shape[0]-1)
-       
     n = len(data)
-    fig, axes = plt.subplots(ns, n, figsize=(figsize[0]*n, figsize[1]*ns))
-    axes = axes.flatten()
+    ns = len(slices)
     
-    for idx, axis in enumerate(axes):
-        slice_idx = idx // n
-        data_idx  = idx % n
-        axis.imshow(data[data_idx][slice_idx], cmap=cmap)
+    for i in range(ns):
+        slices[i] = slices[i] if slices[i] != -1 else random.randint(0, data[0].shape[0]-1)
+
+    nx, ny = ns, n
+    if transpose:
+        nx, ny = ny, nx
+    fig, axes = plt.subplots(nx, ny, figsize=(figsize[0]*ny, figsize[1]*nx))
+    
+    axes = [axes] if nx*ny==1 else axes.flatten()
+    
+    for i, axis in enumerate(axes):
+        ix, iy = i // ny, i % ny
+        if transpose:
+            ix, iy = iy, ix
+        axis.imshow(data[iy][slices[ix]], cmap=cmap)
         axis.axis('off')
 
     if show_titles:
-        for idx, axis in enumerate(axes):
-            slice_idx = idx // n
-            data_idx  = idx % n
-            title = f'{titles[data_idx]} ' if data_idx < len(titles) else f''
-            title += f' [{slices[slice_idx]}]' if show_ids else '' 
+        for i, axis in enumerate(axes):
+            ix, iy = i // ny, i % ny
+            if transpose:
+                ix, iy = iy, ix
+            title = f'{titles[iy]} ' if iy < len(titles) else f''
+            title += f' [{slices[ix]}]' if show_ids else ''
             axis.title.set_text(title)
     
     plt.show()
